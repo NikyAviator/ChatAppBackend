f34f52f1d6de2c2b0713577f0824a43908794935
Nikolai Kocev <nikolai_kocev@hotmail.com>
2022-11-14 16:27:58 +0100

    Initialized an empty Maven Project

---
+++ chatappbackend/src/main/java/ppc/chatappbackend/ChatappbackendApplication.java
@@ -0,0 +1,13 @@
+package ppc.chatappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatappbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatappbackendApplication.class, args);
+	}
+
+}
